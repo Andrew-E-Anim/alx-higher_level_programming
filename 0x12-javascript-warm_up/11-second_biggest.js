ce3226f1250cb75f6e811064e6beb1c60d1947f2
Andrew Anim <andrew.eanim@gmail.com>
2024-04-11 11:58:46 -0700

    0x12. JavaScript - Warm up

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
-if (process.argv.length < 4) {
+if (process.argv.length <= 3) {
   console.log('0');
 } else {
-  const x = process.argv.slice(2);
-  x.sort((a, b) => b - a);
-  console.log(x[1]);
+  const arr = process.argv.slice(2).map(Number);
+  const second = arr.sort(function (a, b) { return b - a; })[1];
+  console.log(second);
 }
